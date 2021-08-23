752435ed1987bd6a9435e043fabcb387561968cb
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-08-24 08:39:01 +0900

    データを取得・モデルに引き渡し

--- src/main/java/com/example/test/controller/PostsController.java
+++ src/main/java/com/example/test/controller/PostsController.java
@@ -1,5 +1,10 @@
 package com.example.test.controller;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +13,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/posts")
 public class PostsController {
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
+
   @GetMapping("/index")
   public String index(Model model) {
+    String sql = "SELECT * FROM message_table";
+    List<Map<String, Object>> list = jdbcTemplate.queryForList(sql);
+    model.addAttribute("dataList", list);
     model.addAttribute("describe", "ここでは新規投稿ができます");
     return "posts/index";
   }
