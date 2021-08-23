67b810ba9b82b486fdbc11860c174036297a922c
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-08-23 18:31:35 +0900

    リクエストスコープを定義

--- src/main/java/com/example/test/controller/PostsController.java
+++ src/main/java/com/example/test/controller/PostsController.java
@@ -1,6 +1,7 @@
 package com.example.test.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/posts")
 public class PostsController {
   @GetMapping("/index")
-  public String index() {
+  public String index(Model model) {
+    model.addAttribute("describe", "ここでは新規投稿ができます");
     return "posts/index";
   }
 }
