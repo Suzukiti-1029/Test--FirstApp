5a5be0bdbc537337318d8fafb178ae994b1dad79
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-08-24 18:28:45 +0900

    投稿ページの作成・遷移

--- src/main/java/com/example/test/controller/PostsController.java
+++ src/main/java/com/example/test/controller/PostsController.java
@@ -8,6 +8,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,12 +22,18 @@ public class PostsController {
     String sql = "SELECT * FROM message_table ORDER BY id DESC";
     List<Map<String, Object>> list = jdbcTemplate.queryForList(sql);
     model.addAttribute("dataList", list);
-    model.addAttribute("describe", "ここでは新規投稿ができます");
+    model.addAttribute("describe", "ここでは投稿一覧が見れます");
     return "posts/index";
   }
 
-  @GetMapping("/post")
+  @GetMapping("/new")
   public String post(Model model) {
+    model.addAttribute("describe", "ここでは新規投稿ができます");
+    return "posts/new";
+  }
+
+  @GetMapping("/post")
+  public String create(Model model) {
     model.addAttribute("describe", "入力の保存が完了しました");
     return "posts/post";
   }
