7de3655ce45bedb7f3c1d69eefc15b5b53186fe5
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-08-24 18:45:44 +0900

    Formクラスの生成

---
+++ src/main/java/com/example/test/controller/Form.java
@@ -0,0 +1,13 @@
+package com.example.test.controller;
+
+public class Form {
+  private int id;
+  private String message;
+
+  // Getter
+  public int getId() { return this.id; }
+  public String getMessage() { return this.message; }
+
+  // Setter
+  public void setMessage(String message) { this.message = message; }
+}
