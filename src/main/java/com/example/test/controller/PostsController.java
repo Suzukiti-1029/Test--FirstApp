be5ab2887ffd170da5d3e2fc01826c48214c0c24
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-08-24 18:58:27 +0900

    投稿フォーム作成

--- src/main/java/com/example/test/controller/PostsController.java
+++ src/main/java/com/example/test/controller/PostsController.java
@@ -8,6 +8,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -27,7 +28,7 @@ public class PostsController {
   }
 
   @GetMapping("/new")
-  public String post(Model model) {
+  public String post(@ModelAttribute Form form, Model model) {
     model.addAttribute("describe", "ここでは新規投稿ができます");
     return "posts/new";
   }
